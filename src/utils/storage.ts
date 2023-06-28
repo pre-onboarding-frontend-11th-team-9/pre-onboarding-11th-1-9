3177a4002594f059a0cbae03527c4ff69aee59e1
BRWEB <52031484+rockbell89@users.noreply.github.com>
2023-06-29 02:12:49 +0900

    feat: localStorage 및 validation utils 함수 추가

---
+++ src/utils/storage.ts
@@ -0,0 +1,15 @@
+const TOKEN_NAME = 'access_token';
+
+const storage = {
+  getToken() {
+    return localStorage.getItem(TOKEN_NAME);
+  },
+  setToken(token: string) {
+    localStorage.setItem(TOKEN_NAME, token);
+  },
+  removeToken() {
+    localStorage.removeItem(TOKEN_NAME);
+  },
+};
+
+export default storage;
