b4244fa8abec179dd2f7bb0cec72c3a486c76551
LEE DONG GYU <tenenger7125@gmail.com>
2023-06-29 22:26:57 +0900

    feat: update try-catch syntax remove
    Co-authored-by: 조은선 <joeunseon125@gmail.com>
    Co-authored-by: 토끼리 <reezekim@users.noreply.github.com>

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,24 +1,14 @@
 import { API } from '../constants';
 import apiClient from '../utils/apiClient';
-import storage from '../utils/storage';
 
 const authService = {
   async signIn(signData) {
-    try {
-      const response = await apiClient.post(API.SIGNIN, signData);
-      storage.setToken(response.data.access_token);
-      return response;
-    } catch (err) {
-      console.error(err);
-    }
+    const response = await apiClient.post(API.SIGNIN, signData);
+    return response;
   },
   async signUp(signData) {
-    try {
-      const response = await apiClient.post(API.SIGNUP, signData);
-      return response;
-    } catch (err) {
-      console.error(err);
-    }
+    const response = await apiClient.post(API.SIGNUP, signData);
+    return response;
   },
 };
 
