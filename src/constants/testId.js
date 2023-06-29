148e3cfae8c5fa6bf835f03e3e7523e561aa8124
LEE DONG GYU <tenenger7125@gmail.com>
2023-06-29 19:26:54 +0900

    feat: add test id constants

---
+++ src/constants/testId.js
@@ -0,0 +1,13 @@
+export const TEST_ID = {
+  EMAIL_INPUT: 'email-input',
+  PASSWORD_INPUT: 'password-input',
+  MODIFY_INPUT: 'modify-input',
+  NEW_TODO_INPUT: 'new-todo-input',
+  SIGNUP_BUTTON: 'signup-button',
+  SIGNIN_BUTTON: 'signin-button',
+  MODIFY_BUTTON: 'modify-button',
+  DELETE_BUTTON: 'delete-button',
+  SUBMIT_BUTTON: 'submit-button',
+  CANCEL_BUTTON: 'cancel-button',
+  NEW_TODO_ADD_BUTTON: 'new-todo-add-button',
+};
