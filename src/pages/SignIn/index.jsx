a738045f2e8dc07f6f7a24a3ee019e4039adfee0
LEE DONG GYU <tenenger7125@gmail.com>
2023-06-29 22:27:19 +0900

    feat: add signin/signup pages
    Co-authored-by: 조은선 <joeunseon125@gmail.com>
    Co-authored-by: 토끼리 <reezekim@users.noreply.github.com>

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,22 +1,49 @@
 import { useNavigate } from 'react-router-dom/dist';
-import authService from '../../services/auth';
+import { useForm } from '../../hooks/useForm';
+import validate from '../../utils/validate';
+import { PATH } from '../../constants';
+
+const { email: emailValidate, password: passwordValidate } = validate;
 
 const SignInPage = () => {
+  const { form, handleInputChange, handleSignInSubmit } = useForm({
+    email: '',
+    password: '',
+  });
+
+  const isDisabled = !(
+    emailValidate(form.email) && passwordValidate(form.password)
+  );
+
   const navigate = useNavigate();
-  const handleSignInBtnClick = async () => {
-    const token = await authService.signIn({
-      email: 'test@wanted.com',
-      password: '12345678',
-    });
-    console.log(token);
-    if (token) {
-      navigate('/todo');
-    }
-  };
 
   return (
     <div>
-      <button onClick={handleSignInBtnClick}>로그인</button>
+      <form onSubmit={handleSignInSubmit}>
+        <h2>로그인</h2>
+        <input
+          data-testid="email-input"
+          type="text"
+          name="email"
+          placeholder="이메일을 입력해주세요"
+          value={form.email}
+          onChange={handleInputChange}
+        />
+        <input
+          data-testid="password-input"
+          type="password"
+          name="password"
+          placeholder="비밀번호를 입력해주세요"
+          value={form.password}
+          onChange={handleInputChange}
+        />
+        <button data-testid="signin-button" type="submit" disabled={isDisabled}>
+          로그인
+        </button>
+      </form>
+      <button type="button" onClick={() => navigate(PATH.SIGN_UP)}>
+        회원가입 하러가기
+      </button>
     </div>
   );
 };
